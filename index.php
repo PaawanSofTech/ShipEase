35be2358e6f7f8ae0f4cb2573615e1923fb94b09
PaawanSofTech~ <animemines2004@gmail.com>
2024-03-05 12:16:57 +0530

    home_edit

--- index.php
+++ index.php
@@ -90,116 +90,7 @@ include('includes/config.php');
 		</div>
 	</div>
 
-	<section class="ftco-section ftco-no-pb ftco-no-pt">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="search-wrap-1 ftco-animate mb-5">
-						<form action="#" class="search-property-1">
-							<div class="row">
-								<div class="col-lg align-items-end">
-									<div class="form-group">
-										<label for="#">Select Model</label>
-										<div class="form-field">
-											<div class="select-wrap">
-												<div class="icon"><span class="ion-ios-arrow-down"></span></div>
-												<select name="" id="" class="form-control">
-													<option value="">Select Model</option>
-													<option value="">Model 1</option>
-													<option value="">Model 2</option>
-													<option value="">Model 3</option>
-													<option value="">Model 4</option>
-													<option value="">Model 5</option>
-													<option value="">Model 6</option>
-													<option value="">Model 7</option>
-													<option value="">Model 8</option>
-													<option value="">Model 9</option>
-													<option value="">Model 10</option>
-												</select>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-lg align-items-end">
-									<div class="form-group">
-										<label for="#">Select Brand</label>
-										<div class="form-field">
-											<div class="select-wrap">
-												<div class="icon"><span class="ion-ios-arrow-down"></span></div>
-												<select name="" id="" class="form-control">
-													<option value="">Select Brand</option>
-													<option value="">Brand 1</option>
-													<option value="">Brand 2</option>
-													<option value="">Brand 3</option>
-													<option value="">Brand 4</option>
-													<option value="">Brand 5</option>
-													<option value="">Brand 6</option>
-													<option value="">Brand 7</option>
-													<option value="">Brand 8</option>
-													<option value="">Brand 9</option>
-													<option value="">Brand 10</option>
-												</select>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-lg align-items-end">
-									<div class="form-group">
-										<label for="#">Year Model</label>
-										<div class="form-field">
-											<div class="select-wrap">
-												<div class="icon"><span class="ion-ios-arrow-down"></span></div>
-												<select name="" id="" class="form-control">
-													<option value="">Year Model</option>
-													<option value="">2019</option>
-													<option value="">2018</option>
-													<option value="">2017</option>
-													<option value="">2016</option>
-													<option value="">2015</option>
-													<option value="">2014</option>
-												</select>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-lg align-items-end">
-									<div class="form-group">
-										<label for="#">Price Limit</label>
-										<div class="form-field">
-											<div class="select-wrap">
-												<div class="icon"><span class="ion-ios-arrow-down"></span></div>
-												<select name="" id="" class="form-control">
-													<option value="">$1</option>
-													<option value="">$50</option>
-													<option value="">$100</option>
-													<option value="">$200</option>
-													<option value="">$300</option>
-													<option value="">$400</option>
-													<option value="">$500</option>
-													<option value="">$600</option>
-													<option value="">$700</option>
-													<option value="">$800</option>
-													<option value="">$900</option>
-													<option value="">$1000</option>
-												</select>
-											</div>
-										</div>
-									</div>
-								</div>
-								<div class="col-lg align-self-end">
-									<div class="form-group">
-										<div class="form-field">
-											<input type="submit" value="Search" class="form-control btn btn-primary">
-										</div>
-									</div>
-								</div>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
+	<br>
 
 	<section class="ftco-section services-section ftco-no-pt ftco-no-pb">
 		<div class="container">
@@ -215,10 +106,11 @@ include('includes/config.php');
 						<div class="media-body py-md-4">
 							<div class="d-flex mb-3 align-items-center">
 								<div class="icon"><span class="flaticon-customer-support"></span></div>
-								<h3 class="heading mb-0 pl-3">24/7 Car Support</h3>
+								<h3 class="heading mb-0 pl-3">24/7 Cargo Support</h3>
 							</div>
-							<p>A small river named Duden flows by their place and supplies it with you</p>
-						</div>
+							<p>Experience reliable cargo transportation around the clock.
+							</p>
+							</div>
 					</div>
 				</div>
 				<div class="col-md-3 d-flex align-self-stretch ftco-animate">
@@ -226,10 +118,11 @@ include('includes/config.php');
 						<div class="media-body py-md-4">
 							<div class="d-flex mb-3 align-items-center">
 								<div class="icon"><span class="flaticon-route"></span></div>
-								<h3 class="heading mb-0 pl-3">Lots of location</h3>
+								<h3 class="heading mb-0 pl-3">Diverses location</h3>
 							</div>
-							<p>A small river named Duden flows by their place and supplies it with you</p>
-						</div>
+							<p>Connect effortlessly to various destinations with our comprehensive service.
+								</p>
+							</div>
 					</div>
 				</div>
 				<div class="col-md-3 d-flex align-self-stretch ftco-animate">
@@ -237,10 +130,11 @@ include('includes/config.php');
 						<div class="media-body py-md-4">
 							<div class="d-flex mb-3 align-items-center">
 								<div class="icon"><span class="flaticon-online-booking"></span></div>
-								<h3 class="heading mb-0 pl-3">Reservation</h3>
+								<h3 class="heading mb-0 pl-3">Effortless Reservations</h3>
 							</div>
-							<p>A small river named Duden flows by their place and supplies it with you</p>
-						</div>
+							<p>Book with ease, ensuring a smooth journey every time.
+							</p>
+							</div>
 					</div>
 				</div>
 				<div class="col-md-3 d-flex align-self-stretch ftco-animate">
@@ -250,8 +144,9 @@ include('includes/config.php');
 								<div class="icon"><span class="flaticon-rent"></span></div>
 								<h3 class="heading mb-0 pl-3">Rental Cars</h3>
 							</div>
-							<p>A small river named Duden flows by their place and supplies it with you</p>
-						</div>
+							<p>Embark on your journey with our rental cars, providing vitality and mobility for your travels.
+							</p>
+							</div>
 					</div>
 				</div>
 			</div>
